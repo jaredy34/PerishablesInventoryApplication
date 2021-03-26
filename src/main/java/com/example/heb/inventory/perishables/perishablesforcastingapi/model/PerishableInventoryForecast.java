b74cef4b5401e90bd59e048c2e0b1f49e6deb7ce
Jared York <jyorkdev@gmail.com>
2021-03-26 09:00:53 -0500

    add constraints to PerishableInventoryForecast model obj

--- src/main/java/com/example/heb/inventory/perishables/perishablesforcastingapi/model/PerishableInventoryForecast.java
+++ src/main/java/com/example/heb/inventory/perishables/perishablesforcastingapi/model/PerishableInventoryForecast.java
@@ -4,12 +4,15 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+
 @RequiredArgsConstructor
 @Getter @ToString
 public class PerishableInventoryForecast {
-    private final String perishableName;
-    private final Long perishableId;
-    private final Long storeId;
-    private final int amount;
+    private final @NotNull String perishableName;
+    private final @NotNull Long perishableId;
+    private final @Digits(integer = 12, fraction = 0) Long storeId;
+    private final @Digits(integer = 5, fraction = 0) int amount;
     private final String inventoryUnitType = "pallet";
 }
